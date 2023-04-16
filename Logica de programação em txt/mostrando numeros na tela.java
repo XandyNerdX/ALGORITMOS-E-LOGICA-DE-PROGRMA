c13d544d465f0846e6e7c921eed4afd229e71120
Alexandre Silva Ferreira <alexandre.ferreira3628@soupromove.com.br>
2023-04-16 18:08:50 -0300

    Logica de programação, feitas durante as aulas!

---
+++ Logica de programação em txt/mostrando numeros na tela.java
@@ -0,0 +1,20 @@
+public class Text02
+/*  
+    Titulo do programa: Texto 02 Orancle
+    Programador: Alexandre Silva Ferreira
+    Data: 13/03/2023
+*/
+{      
+     //Priimeiro bloco de execução
+    public static void main(String[] args) 
+    {   
+        //inicio do programa
+        System.out.println("1"); 
+        System.out.println("2"); 
+        System.out.println("3");
+        System.out.println ("4");
+        //final do programa
+    }
+    //  Fim do bloco de conclusão
+
+} 
