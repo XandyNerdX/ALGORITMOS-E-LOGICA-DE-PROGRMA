b1dac0f8376d6e5ca642d2098c8440248f63f1cd
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-17 19:24:29 -0300

    Add files via upload

---
+++ Aula.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula;
+
+/**
+ *
+ * @author netbeans
+ */
+public class Aula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           
+        
+        
+        System.out.println("Meu nome é Alexandre");
+        System.out.println("Alexandre é muito bacana");
+        System.out.println("Viva o Alexandre");
+        System.out.println("Aproveite a Avaliação de Alexandre "
+            + "Dia! Meu nome é Alexandre. Sei como todos estão "
+            + "interessados em "
+            + "começar a apreciar o Alexandre no dia da "
+            + "Avaliação do Alexandre! Alexandre, Alexandre! Sim "
+            + "Alex!!! Este sou eu! Alexandre é o melhor encontro de "
+            + "todos      os tempos");
+        
+        String x = "";
+        
+        /*
+            Alexndre
+        */
+        
+        x = "Alexandre";
+        
+        
+        System.out.println("Meu nome é " + x);
+        System.out.println(x + "e muito bacana!");
+        System.out.println("Viva " +  x + "!");
+        System.out.println("Aproveite e Avaliação de " + x + " "
+                + "Dia! Meu nome é " + x + ". Sei como todos estão "
+                + "interessados em"
+                + "começar a apreciar " + x +" no Dia da avaliação de "
+                + x + "! " + x + ", " + x + "! Sim "
+                + x + "!!! Este sou eu! " + x +"é o melhor pretendente "
+                + " de todos os tempos!");
+        
+        /*
+            SAM
+        */
+        
+        x = "KIT"; 
+        
+                
+        System.out.println("Meu nome é " + x);
+        System.out.println(x + "e muito bacana!");
+        System.out.println("Viva " +  x + "!");
+        System.out.println("Aproveite e Avaliação de " + x + " "
+                + "Dia! Meu nome é " + x + ". Sei como todos estão "
+                + "interessados em"
+                + "começar a apreciar " + x +" no Dia da avaliação de "
+                + x + "! " + x + ", " + x + "! Sim "
+                + x + "!!! Este sou eu! " + x +"é o melhor pretendente "
+                + " de todos os tempos!");
+        
+       /*
+            NICK
+        */
+        
+        x = "Nicky"; 
+        
+                
+        System.out.println("Meu nome é " + x);
+        System.out.println(x + "e muito bacana!");
+        System.out.println("Viva " +  x + "!");
+        System.out.println("Aproveite e Avaliação de " + x + " "
+                + "Dia! Meu nome é " + x + ". Sei como todos estão "
+                + "interessados em"
+                + "começar a apreciar " + x +" no Dia da avaliação de "
+                + x + "! " + x + ", " + x + "! Sim "
+                + x + "!!! Este sou eu! " + x +"é o melhor pretendente "
+                + " de todos os tempos!");
+        
+        /*
+            Professor
+        */
+        
+        x = "Professor";
+
+        
+        System.out.println("Meu nome é " + x);
+        System.out.println(x + "e muito bacana!");
+        System.out.println("Viva " +  x + "!");
+        System.out.println("Aproveite e Avaliação de " + x + " "
+                + "Dia! Meu nome é " + x + ". Sei como todos estão "
+                + "interessados em"
+                + "começar a apreciar " + x +" no Dia da avaliação de "
+                + x + "! " + x + ", " + x + "! Sim "
+                + x + "!!! Este sou eu! " + x +"é o melhor pretendente "
+                + " de todos os tempos!");        
+        
+            
+        
+        /*
+        
+        
+        */
+        
+        int y = 20;
+           System.out.println(y); 
+           
+        double z = 23.0;
+            System.err.println(z);
+            
+        boolean Hacker1337 = true;
+            
+       /*
+        Testando atribuição de variavel
+        */
+       
+       int i = 20;
+           i = 25;
+           i = 5 + 3;
+           i = i + 1;
+           i += 1;
+           i++;
+      System.out.println(i);     
+      
+      
+        
+
+    }
+ 
+    
+}
