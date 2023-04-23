9318f6d3a0ab74366b4922b26980af3c8ac0c854
Alexandre Silva Ferreira <alexandre.ferreira3628@soupromove.com.br>
2023-04-23 17:42:43 -0300

    Exercicios de programação feitos durante a aula! (Algoritmo e Lófica de Programação)

---
+++ Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicios_sesao04_orancle;
+
+/**
+ *
+ * @author Alexandre Silva
+ */
+public class Exercicios_Sesao04_Orancle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
