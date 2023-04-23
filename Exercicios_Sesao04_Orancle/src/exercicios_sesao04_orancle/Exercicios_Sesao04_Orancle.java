0ebc1028efab1e2c8be07301022607bc3222507a
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-23 20:05:03 -0300

    03

--- Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
+++ Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
- package exercicios_sesao04_orancle;
-=======
  package exercicios_sesao04_orancle;
 
 import java.util.Scanner;
@@ -13,28 +10,15 @@ public class Exercicios_Sesao04_Orancle {
         int f = 1000;
         System.out.println("celsius: " + (f-32)* 5/9);
     */
->>>>>>> c042ec1b4864546c507891a47026efbf1415a776
 
    //⦁	Converte a temperatura fornecida de Fahrenheit para Celsius.
 
-<<<<<<< HEAD
+
 public class Exercicios_Sesao04_Orancle {
 
     /*
     Formula:
-=======
-    
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-            System.out.print("a TEMPERATURA EM Fahrenheit: ");
-            int f = scanner.nextInt();
-            System.out.pr
-            
-            intln("Temperatura em Fahrenheit: " + (f-32)* 5/9);
->>>>>>> c042ec1b4864546c507891a47026efbf1415a776
 
-<<<<<<< HEAD
         int f = 1000;
         System.out.println("celsius: " + (f-32)* 5/9);
     */
