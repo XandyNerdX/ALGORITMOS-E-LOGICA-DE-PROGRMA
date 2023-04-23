c042ec1b4864546c507891a47026efbf1415a776
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-23 19:44:44 -0300

    //⦁	Calcula o tamanho da hipotenusa de um triângulo dado o comprimento de seus lados.

--- Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
+++ Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
@@ -10,13 +10,25 @@ public class Exercicios_Sesao04_Orancle {
         int f = 1000;
         System.out.println("celsius: " + (f-32)* 5/9);
     */
+
+   //⦁	Converte a temperatura fornecida de Fahrenheit para Celsius.
+
     
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
             System.out.print("a TEMPERATURA EM Fahrenheit: ");
             int f = scanner.nextInt();
-            System.out.println("Temperatura em Fahrenheit: " + (f-32)* 5/9);
+            System.out.pr
+            
+            intln("Temperatura em Fahrenheit: " + (f-32)* 5/9);
+
+
+    //⦁	Calcula o tamanho da hipotenusa de um triângulo dado o comprimento de seus lados.
+
+    Scanner scanner = new Scanner(System.in);
+    
+
 
     }
    }
