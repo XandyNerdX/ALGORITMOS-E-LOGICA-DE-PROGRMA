1454a5ac220e6a0d54ec124f6e9e1f7c9592cb5b
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-23 19:55:36 -0300

    Calcula o tamanho da hipotenusa de um triângulo dado o comprimento de seus lados.02

--- Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
+++ Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+ package exercicios_sesao04_orancle;
+=======
  package exercicios_sesao04_orancle;
 
 import java.util.Scanner;
@@ -10,9 +13,16 @@ public class Exercicios_Sesao04_Orancle {
         int f = 1000;
         System.out.println("celsius: " + (f-32)* 5/9);
     */
+>>>>>>> c042ec1b4864546c507891a47026efbf1415a776
 
    //⦁	Converte a temperatura fornecida de Fahrenheit para Celsius.
 
+<<<<<<< HEAD
+public class Exercicios_Sesao04_Orancle {
+
+    /*
+    Formula:
+=======
     
     public static void main(String[] args) {
         
@@ -22,12 +32,34 @@ public class Exercicios_Sesao04_Orancle {
             System.out.pr
             
             intln("Temperatura em Fahrenheit: " + (f-32)* 5/9);
+>>>>>>> c042ec1b4864546c507891a47026efbf1415a776
+
+<<<<<<< HEAD
+        int f = 1000;
+        System.out.println("celsius: " + (f-32)* 5/9);
+    */
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("a TEMPERATURA EM Fahrenheit: ");
+            int f = scanner.nextInt();
+            System.out.println("Temperatura em Fahrenheit: " + (f-32)* 5/9);
 
 
     //⦁	Calcula o tamanho da hipotenusa de um triângulo dado o comprimento de seus lados.
 
     Scanner scanner = new Scanner(System.in);
-    
+        System.out.PRINT.("Comprimento do primeiro cateto: ");
+        double cateto1 = scanner.nextInt();
+        System.out.PRINT.("Comprimento do segundo cateto: ");
+        double cateto2 = scanner.nextInt();
+        double hipotenusa = cateto1*cateto1 + cateto2*cateto*2;
+
+
+        System.out.print("Hipotenusa: " + hipotenusa);
+
+
 
 
     }
