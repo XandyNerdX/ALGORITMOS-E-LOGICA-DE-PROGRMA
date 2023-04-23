09a409430b355d031c6f5e596c28f32dfb542cb8
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-23 19:41:58 -0300

    Menas linhas de codigos

--- Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
+++ Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
@@ -1,43 +1,24 @@
-/* package exercicios_sesao04_orancle;
-
-import java.util.Scanner;
-
-public class Exercicios_Sesao04_Orancle {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite um número inteiro: ");
-        int numero = scanner.nextInt();
-        System.out.println("O número digitado foi: " + numero);
-    }
-
-}
-*/
  package exercicios_sesao04_orancle;
 
 import java.util.Scanner;
 
 public class Exercicios_Sesao04_Orancle {
 
-    public static void main(Stri10
-    ng[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("a TEMPERATURA EM Fahrenheit: ");
-        int f = scanner.nextInt();
-        int c;
-
-        c = 5 / 9 * (f - 32);
-
-        System.out.println("Temperatura em Fahrenheit " + c);
-        
-
-               //temperatura 
+    /*
+    Formula:
 
         int f = 1000;
         System.out.println("celsius: " + (f-32)* 5/9);
+    */
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("a TEMPERATURA EM Fahrenheit: ");
+            int f = scanner.nextInt();
+            System.out.println("Temperatura em Fahrenheit: " + (f-32)* 5/9);
 
     }
-
-}
+   }
 
 
