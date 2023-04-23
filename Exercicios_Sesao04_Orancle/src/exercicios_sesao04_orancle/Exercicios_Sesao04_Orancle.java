35ee404eabd288dc6331d5cd48e005a253c4ffa0
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-23 20:06:44 -0300

    chat gpt (consertou codigo)

--- Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
+++ Exercicios_Sesao04_Orancle/src/exercicios_sesao04_orancle/Exercicios_Sesao04_Orancle.java
@@ -1,4 +1,4 @@
- package exercicios_sesao04_orancle;
+package exercicios_sesao04_orancle;
 
 import java.util.Scanner;
 
@@ -6,48 +6,25 @@ public class Exercicios_Sesao04_Orancle {
 
     /*
     Formula:
-
         int f = 1000;
         System.out.println("celsius: " + (f-32)* 5/9);
     */
 
-   //⦁	Converte a temperatura fornecida de Fahrenheit para Celsius.
-
-
-public class Exercicios_Sesao04_Orancle {
-
-    /*
-    Formula:
-
-        int f = 1000;
-        System.out.println("celsius: " + (f-32)* 5/9);
-    */
-    
+    //⦁	Converte a temperatura fornecida de Fahrenheit para Celsius.
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
-            System.out.print("a TEMPERATURA EM Fahrenheit: ");
-            int f = scanner.nextInt();
-            System.out.println("Temperatura em Fahrenheit: " + (f-32)* 5/9);
+        System.out.print("a TEMPERATURA EM Fahrenheit: ");
+        int f = scanner.nextInt();
+        System.out.println("Temperatura em Fahrenheit: " + (f-32)* 5/9);
 
+        //⦁	Calcula o tamanho da hipotenusa de um triângulo dado o comprimento de seus lados.
+        System.out.print("Comprimento do primeiro cateto: ");
+        double cateto1 = scanner.nextDouble();
 
-    //⦁	Calcula o tamanho da hipotenusa de um triângulo dado o comprimento de seus lados.
+        System.out.print("Comprimento do segundo cateto: ");
+        double cateto2 = scanner.nextDouble();
 
-         Scanner scanner = new Scanner(System.in);
-            System.out.print("Comprimento do primeiro cateto: ");
-                double cateto1 = scanner.nextInt();
-            
-            System.out.print("Comprimento do segundo cateto: ");
-                double cateto2 = scanner.nextInt();
-            
-            double hipotenusa = Math.sqrt(cateto1*cateto1 + cateto2*cateto2);
-                System.out.print("Hipotenusa: " + hipotenusa);
-
-
-
-
-
+        double hipotenusa = Math.sqrt(cateto1*cateto1 + cateto2*cateto2);
+        System.out.print("Hipotenusa: " + hipotenusa);
     }
-   }
-
-
+}
