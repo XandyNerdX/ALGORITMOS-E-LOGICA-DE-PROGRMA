f3a9c16d4b2423b1843d911255ec40a2587ec0e2
Alexandre Silva Ferreira <alexandre.ferreira3628@soupromove.com.br>
2023-04-24 23:25:36 -0300

    Versão01

--- Orancle 3-X/Exercicio4.java
+++ Orancle 3-X/Exercicio4.java
@@ -1,11 +1,22 @@
-package exercicios_sesao04_orancle;
-
-import java.util.Random;
 import java.util.Scanner;
 
-public class Exercicios_Sesao04_Orancle {
+public class Input04 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(Input04.class.getResourceAsStream("input04text.txt"));      
+        
+        //Edit these lines to advance the scanner
+        sc.nextLine();
+        System.out.println(sc.nextLine());
 
-    
-
-
+        
+        
+        //Does this line contain "BlueBumper"?
+        System.out.println(sc.findInLine("BlueBumper"));
+        //Store the next two numbers as xPosition and yPosition
+        //Print these positions
+        
+        
+        System.out.println("X: "        +", Y: "        );
+        sc.close();
+    }    
 }
