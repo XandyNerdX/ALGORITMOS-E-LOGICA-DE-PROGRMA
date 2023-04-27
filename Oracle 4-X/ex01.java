12c33724063a59253955b26996bbf2a530c14976
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-04-26 23:25:32 -0300

    Pagina 19 (Orancle 4-1)

---
+++ Oracle 4-X/ex01.java
@@ -0,0 +1,61 @@
+public class ex01 {
+    //Campos: 
+    public double tax = 0.05;
+    public double tip = 0.15;
+    public double originalPrice = 10;   
+
+    public void findTotal() {
+        //Calcula o total depois da taxa e da gorjeta 
+        //Imprime esse valor 
+    }//fim do metodo 
+
+    public static void main(String[] args) {
+/* 
+        double total1 = person1 * (1 + tax + tip);
+        System.out.println(total); */
+        //fim do metodo main    
+
+
+       /* FORMA BURRA DE FAZER: (OBS: EU FIZ DA FORMA BURRA :P )
+
+    int pessoas = 8;
+    double totalconta = 0;
+    double imposto = .05;
+    double gorjeta = .15;
+
+        double pessoa1 = 10;  
+        double total1 = pessoa1*(1 + imposto + gorjeta);
+        System.out.println(total1);
+        
+        double pessoa2 = 14.4;
+        double total2 = pessoa2(1 + imposto + gorjeta);
+        System.out.println(total2);
+
+        double pessoa3 = 10.0
+        double total3 = pessoa3(1 + imposto + gorjeta);
+        System.out.println(total3);
+
+        double pessoa4 = 9.6;
+        double total3 = pessoa4(1 + imposto + gorjeta);
+        System.out.println(total4);
+
+        double pessoa5 = 8.4; 
+        double total3 = pessoa5(1 + imposto + gorjeta);
+        System.out.println(total5);
+        
+        double pessoa6 = 18.0;
+        double total3 = pessoa6(1 + imposto + gorjeta);
+        System.out.println(total6);
+
+        double pessoa7 = 13.2;
+        double total3 = pessoa7(1 + imposto + gorjeta);
+        System.out.println(total7);
+
+        double pessoa8 = 36.0;  
+        double total3 = pessoa8(1 + imposto + gorjeta);
+        System.out.println(total8);
+  */
+
+
+    }
+}
