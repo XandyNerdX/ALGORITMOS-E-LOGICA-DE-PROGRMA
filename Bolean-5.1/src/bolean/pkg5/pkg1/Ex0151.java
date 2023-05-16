1a2756f2f8d7a3270ef44207dbc87c5bec0a6202
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-15 21:02:22 -0300

    Boolean
    Par & Impar 
    Dentre outros dos slides!

---
+++ Bolean-5.1/src/bolean/pkg5/pkg1/Ex0151.java
@@ -0,0 +1,22 @@
+package ex01.pkg5.pkg1;
+
+import java.util.Scanner;
+
+public class Ex0151 {
+
+
+    public static void main(String[] args) {
+        
+       
+        
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe sua idade: ");
+        int x = scanner.nextInt();
+        
+        boolean drivingUnderAge = false;
+        System.out.println(x <= 18);
+         
+
+    }
+    
+}
