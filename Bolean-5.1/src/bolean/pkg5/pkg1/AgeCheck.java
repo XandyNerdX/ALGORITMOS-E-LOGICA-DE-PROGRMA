3ba28bb89698a6ead5ce040eddac3440b1f539b7
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-15 21:17:52 -0300

    Boolean
    Ex 4 e 5 do slide 5.1 orancle!

---
+++ Bolean-5.1/src/bolean/pkg5/pkg1/AgeCheck.java
@@ -0,0 +1,17 @@
+package ex01.pkg5.pkg1;
+
+public class AgeCheck {
+   
+    public static void main(String[] args) {
+        int myAge = 15;   // Eu tenho 19; deixe-me ver se eu posso dirigir ainda
+        
+        if (myAge >= 16) {
+            System.out.println("Tenho idade para ter carteira de motorista!");
+        }
+        else {
+            System.out.println("Ainda não tenho idade suficiente... :*(");
+        }
+    }
+}
+    
+
