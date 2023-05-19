b6d63fdccc63b7a5b137e029abd819753c18a697
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-19 12:27:41 -0300

    Renomeando arquivos

--- Orancle 5-X/src/bolean/pkg5/bolean5-1/AgeCheck.java
+++ Orancle 5-X/src/bolean/pkg5/bolean5-1/AgeCheck.java
@@ -1,9 +1,15 @@
 package ex01.pkg5.pkg1;
 
+import java.util.Scanner;
+
 public class AgeCheck {
    
     public static void main(String[] args) {
-        int myAge = 15;   // Eu tenho 19; deixe-me ver se eu posso dirigir ainda
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe sua idade: ");
+        int myAge = scanner.nextInt();
+          // Eu tenho 19; deixe-me ver se eu posso dirigir ainda
         
         if (myAge >= 16) {
             System.out.println("Tenho idade para ter carteira de motorista!");
