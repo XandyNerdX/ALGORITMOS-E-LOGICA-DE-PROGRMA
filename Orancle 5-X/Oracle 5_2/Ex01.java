6bf52320fecdc23f1e49faf2a1a2f18caaa1da21
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-29 14:10:53 -0300

    Novo projeto com arquivo do ex1 (INICIO)

---
+++ Orancle 5-X/Oracle 5_2/Ex01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class WatchMovie {
+
+    public static void main(String args[]) {
+        System.out.print("Digite o preço do ingresso de cinema \n");
+
+    }
+}
