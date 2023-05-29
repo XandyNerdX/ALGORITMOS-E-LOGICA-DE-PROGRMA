69e8c8a003dd08a9c39f86023f6083c72f69997c
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-29 14:00:48 -0300

    IMC dando resultado ao contrario!

--- Oracle 4-X/Orancle_4-5/IMC.java
+++ Oracle 4-X/Orancle_4-5/IMC.java
@@ -1,3 +1,4 @@
+import java.net.SocketTimeoutException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
@@ -23,10 +24,31 @@ public class IMC {
             // limitei o número de casas decimais para duas!
 
             System.out.println("Seu IMC: " + imcFormatado);
+
+            // infoma como esta seu peso de acordo com IMC100
+
+            if (imc >= 18.5) {
+                System.out.println("Abaixo do peso normal");
+
+            } else if (imc <= 18.5 && imc >= 24.9) {
+                System.out.println("Peso normal");
+            }
+            if (imc <= 25.0 && imc >= 29.9) {
+                System.out.println("Excesso de peso");
+            } else if (imc <= 30.0 && imc >= 34.9) {
+                System.out.println("Obesidade classe I");
+            }
+            if (imc <= 35.0 && imc >= 39.9) {
+                System.out.println("Obesidade classe II");
+            } else if (imc <= 40) {
+                System.out.println("Obesidade classe III");
+            }
+
         } catch (Exception e) {
             System.out.println("Ocorreu um erro na entrada. Verifique os valores inseridos.");
             // catch O bloco catch é usado em conjunto com o bloco try para capturar
             // exceções (erros)
         }
+
     }
 }
