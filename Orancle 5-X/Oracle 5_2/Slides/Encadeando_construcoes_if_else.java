27266b51dcaa0172b4200b9dc7cf9275ebdd50ed
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-29 15:01:47 -0300

    Pag26

--- Orancle 5-X/Oracle 5_2/Slides/Encadeando_construcoes_if_else.java
+++ Orancle 5-X/Oracle 5_2/Slides/Encadeando_construcoes_if_else.java
@@ -1,11 +1,16 @@
 public class Encadeando_construcoes_if_else {
-if (<condition1>){
-    //code_block1
+
+    public static void main(String args[]) {
+        double income = 30000, tax;
+
+        if (income <= 15000) {
+            tax = 0;
+        } else if (income <= 25000) {
+            tax = 0.05 * (income - 15000);
+        } else {
+            tax = 0.5 * (income - (25000 - 15000));
+            tax += -0.10 * (income - 25000);
+        } // fim if
+    }
+    // Fim do metodo main
 }
-else if (<condition2>){
-    //code_block2
-}
-else {
-    //default_code
-}
-}// fim do if
