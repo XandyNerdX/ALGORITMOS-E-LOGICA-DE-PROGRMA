6d1d2fa0d1a5155e678578f342714c7f60b60892
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-29 13:34:41 -0300

    IMC versão 01

--- Oracle 4-X/Orancle_4-5/IMC.java
+++ Oracle 4-X/Orancle_4-5/IMC.java
@@ -1,4 +1,32 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class IMC {
 
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Informe seu peso: ");
+            double peso = scanner.nextDouble();
+            // Solicita ao usuario que insira sua altura
+
+            System.out.println("Informe sua altura: ");
+            double altura = scanner.nextDouble();
+            // Solicita ao usuario que insira sua altura
+
+            double imc = peso / (altura * altura);
+            // calcula o imc
+
+            DecimalFormat formato = new DecimalFormat("#.##");
+            String imcFormatado = formato.format(imc);
+            // limitei o número de casas decimais para duas!
+
+            System.out.println("Seu IMC: " + imcFormatado);
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro na entrada. Verifique os valores inseridos.");
+            // catch O bloco catch é usado em conjunto com o bloco try para capturar
+            // exceções (erros)
+        }
+    }
 }
