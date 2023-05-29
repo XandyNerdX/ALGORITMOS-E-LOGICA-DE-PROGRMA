0f7fb8b83e088b018ccc4033f388d292cf9a83c0
Alexandre Silva Ferreira <130883702+XandyNerdX@users.noreply.github.com>
2023-05-29 14:01:56 -0300

    Erro consertado (Versão final)

--- Oracle 4-X/Orancle_4-5/IMC.java
+++ Oracle 4-X/Orancle_4-5/IMC.java
@@ -27,20 +27,17 @@ public class IMC {
 
             // infoma como esta seu peso de acordo com IMC100
 
-            if (imc >= 18.5) {
+            if (imc < 18.5) {
                 System.out.println("Abaixo do peso normal");
-
-            } else if (imc <= 18.5 && imc >= 24.9) {
+            } else if (imc >= 18.5 && imc <= 24.9) {
                 System.out.println("Peso normal");
-            }
-            if (imc <= 25.0 && imc >= 29.9) {
+            } else if (imc >= 25.0 && imc <= 29.9) {
                 System.out.println("Excesso de peso");
-            } else if (imc <= 30.0 && imc >= 34.9) {
+            } else if (imc >= 30.0 && imc <= 34.9) {
                 System.out.println("Obesidade classe I");
-            }
-            if (imc <= 35.0 && imc >= 39.9) {
+            } else if (imc >= 35.0 && imc <= 39.9) {
                 System.out.println("Obesidade classe II");
-            } else if (imc <= 40) {
+            } else {
                 System.out.println("Obesidade classe III");
             }
 
