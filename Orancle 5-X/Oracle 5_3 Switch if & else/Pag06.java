92bec9cb725e509f4e15202f1c5844c28b3499a2
Alexandre Silva Ferreira <alexandre.ferreira3628@soupromove.com.br>
2023-05-29 20:40:44 -0300

    Pagina 06 5-3

---
+++ Orancle 5-X/Oracle 5_3 Switch if & else/Pag06.java
@@ -0,0 +1,17 @@
+/*
+ * A Instrução switch
+ * A instrução switch fornece uma sintaxe mais eficiente para escolher entre varias alternativas
+ */
+
+public class Pag06 {
+    public static void main(String[] args) {
+        switch (<variable or expression>) {
+            case <literal value>: //code_block1
+                                  [break;]
+            case <literal value>: //code_block2
+                                  [break;]
+
+            default: //default_code
+        }//end switch
+    }
+}
