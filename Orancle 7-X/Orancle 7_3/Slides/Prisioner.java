6afd04b3e6d077304df014577056415411680eb6
Alexandre Silva Ferreira <alexandre.ferreira3628@soupromove.com.br>
2023-06-25 00:47:33 -0300

    Exercicio01 concluido!

---
+++ Orancle 7-X/Orancle 7_3/Slides/Prisioner.java
@@ -0,0 +1,14 @@
+public class Prisioner {
+    // campos
+    public String name;
+    public double heigt;
+    public int setence;
+
+    // métodos
+
+    
+
+    public void think() {
+        System.out.println("Terei minha vingança.");
+    }// fim do método think
+}// fim da classe prisoner
